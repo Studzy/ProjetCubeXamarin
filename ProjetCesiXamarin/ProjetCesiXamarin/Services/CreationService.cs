4e7d4af5c2a301e3ad040002919f36a14faf4c35
Antoine CACHEUX <coladaitp19-can@ccicampus.fr>
2021-02-11 21:43:58 +0100

    front + viewModel de la creation

---
+++ ProjetCesiXamarin/ProjetCesiXamarin/Services/CreationService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using ProjetCesiXamarin.Constant;
+using ProjetCesiXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+namespace ProjetCesiXamarin.Services
+{
+    class CreationService
+    {
+        HttpClient _client;
+
+        public CreationService()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task GetBaseInfo()
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(ApiProjetCesiConstants.ApiProjetCesiEndpoint + "CreateArticleAPI/GetBaseInfos");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    //ressource = JsonConvert.DeserializeObject<BaseResponse<RessourceData>>(content).Data;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ProjetCesiXamarin/ProjetCesiXamarin/ViewModels/CreationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using ProjetCesiXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCesiXamarin.ViewModels
+{
+    class CreationViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public CreationViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+
+    }
+}
